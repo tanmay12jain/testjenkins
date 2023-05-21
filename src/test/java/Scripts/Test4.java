a7b9fd8a53f66f9356eee529203922c626c960ea
tanmay12jain <tanmayjn1997@gmail.com>
2023-05-21 16:41:47 +0530

    Added 2 more

---
+++ src/test/java/Scripts/Test4.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+ 
+		@Test
+		public void tcl() {
+		Reporter.log("Hello python c", true);
+}
+}
---
+++ src/test/java/Scripts/Test5.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+ 
+		@Test
+		public void tcl() {
+		Reporter.log("Hello python G", true);
+}
+}
